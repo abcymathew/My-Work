f8046d1fb5945a739a4de4630eff8f34bd0f543f
sinju <sinju@DESKTOP-HOO85PR>
2023-11-22 20:59:33 +0530

    first commit

---
+++ static/src/main/java/staticexample/staticexampleclass.java
@@ -0,0 +1,10 @@
+package staticexample;
+
+public class staticexampleclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello world");
+	}
+
+}
